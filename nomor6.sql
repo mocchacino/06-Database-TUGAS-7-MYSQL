52601ced55e563b1700ecf7f04771d68c9fbaa78
wiladhianty <wiladhianty@gmail.com>
2021-07-05 19:37:19 +0700

    tambah tugas

---
+++ nomor6.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.16  Distrib 10.1.19-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	10.1.19-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `info_kota`
+--
+
+DROP TABLE IF EXISTS `info_kota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `info_kota` (
+  `kota` char(20) NOT NULL,
+  `kec` int(11) NOT NULL DEFAULT '1',
+  `kel` int(11) NOT NULL DEFAULT '1',
+  `luas_wilayah` float NOT NULL DEFAULT '1',
+  `jml_penduduk` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`kota`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info_kota`
+--
+
+LOCK TABLES `info_kota` WRITE;
+/*!40000 ALTER TABLE `info_kota` DISABLE KEYS */;
+INSERT INTO `info_kota` VALUES ('Bandung',30,151,167.67,2339463),('Bekasi',12,56,206.61,2381053),('Depok',11,63,200.29,1631951),('Jakarta',44,267,664.01,9988495),('Makassar',14,143,199.26,1651146),('Medan',21,151,265,2465469),('Palembang',14,107,369.22,1548064),('Semarang',16,177,373.78,1621374),('Surabaya',31,154,350.54,2805906),('Tangerang',13,104,153.93,1566190);
+/*!40000 ALTER TABLE `info_kota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `info_kota_2`
+--
+
+DROP TABLE IF EXISTS `info_kota_2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `info_kota_2` (
+  `kota` char(20) NOT NULL,
+  `jml_penduduk` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`kota`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info_kota_2`
+--
+
+LOCK TABLES `info_kota_2` WRITE;
+/*!40000 ALTER TABLE `info_kota_2` DISABLE KEYS */;
+INSERT INTO `info_kota_2` VALUES ('Bandung',2339463),('Bekasi',2381053),('Depok',1631951),('Jakarta',9988495),('Makassar',1651146),('Medan',2465469),('Palembang',1548064),('Semarang',1621374),('Surabaya',2805906),('Tangerang',1566190);
+/*!40000 ALTER TABLE `info_kota_2` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-05 15:01:40
